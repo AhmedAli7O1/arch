f8d988d8faf4e3fc39cf51ae7a23206ac84047d8
Ahmed Ali <bluemax7o1@gmail.com>
2019-05-31 17:17:42 +0200

    init new version project

--- testHelper/exampleLoader.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const path = require('path');
-const _ = require('lodash');
-
-module.exports = function (name, data) {
-  const mod = require(path.resolve(__dirname, 'structureExamples', name));
-  return JSON.parse(_.template(JSON.stringify(mod))(data));
-};
